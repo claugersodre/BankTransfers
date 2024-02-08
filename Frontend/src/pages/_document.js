8642f468649bdd1e0a879ce30d8f4e0d2972881b
claugersodre <claugergregory@gmail.com>
2024-02-08 14:29:53 -0300

    Env removed

--- Frontend/src/pages/_document.js
+++ Frontend/src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Bank Transfers"
+        />
+        <title>Bank Transfers</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
